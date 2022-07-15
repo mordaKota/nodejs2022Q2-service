b706423ed5c1f173487322c9f1a2f9f90807bc73
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-07-15 18:18:19 +0300

    feat: enable swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const root = dirname(__dirname);
+  const doc = await readFile(join(root, 'doc', 'api.yaml'), 'utf-8');
+  SwaggerModule.setup('doc', app, parse(doc));
   await app.listen(4000);
 }
+
 bootstrap();
