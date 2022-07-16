3fc20514908b62eb5f5396ec5cc00ffc48850bf6
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-07-16 17:03:26 +0300

    feat: add basic project structure

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const root = dirname(__dirname);
-  const doc = await readFile(join(root, 'doc', 'api.yaml'), 'utf-8');
+  const doc = await readFile(
+    join(dirname(__dirname), 'doc', 'api.yaml'),
+    'utf-8',
+  );
   SwaggerModule.setup('doc', app, parse(doc));
   await app.listen(4000);
 }
