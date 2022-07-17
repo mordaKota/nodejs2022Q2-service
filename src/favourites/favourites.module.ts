795f78de32bad82b1bf4a6f2afe4a08ec84ee4d5
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-07-17 13:24:49 +0300

    fix: fix for project structure, wrong naming for favorites

--- src/favourites/favourites.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FavouritesService } from './favourites.service';
-import { FavouritesController } from './favourites.controller';
-
-@Module({
-  controllers: [FavouritesController],
-  providers: [FavouritesService],
-})
-export class FavouritesModule {}
