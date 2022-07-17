07d4f660b128eef4d04dfc61428a31d131b09589
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-07-17 20:15:36 +0300

    feat: add postman collection and env config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { readFile } from 'fs/promises';
 import { dirname, join } from 'path';
 import { parse } from 'yaml';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +17,7 @@ async function bootstrap() {
     'utf-8',
   );
   SwaggerModule.setup('doc', app, parse(doc));
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 
 bootstrap();
